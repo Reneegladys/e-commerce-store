f2b3e3582d6c147bf07bbf26443fffbc3683e71e
Renée Gladys <renee16@live.se>
2025-02-18 15:55:29 +0100

    files productcontroller and productroutes are made, even fines the GET request, worked with postman

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,11 @@ const express = require ('express');
 const app = express ();
 
 
+app.use (express.json());
+
+// import all the routes
+const products = require ('./routes/productRoutes')
+
+app.use ('/api/', products)
+
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require ('./app')
-const dotenv = require ('dotenv')
+require('dotenv').config();
 
 
 
 const PORT = process.env.PORT || 5001
+
+console.log('PORT:', process.env.PORT);
+
 
 app.listen (PORT, () => {
     console.log (`server is running on PORT: ${PORT}`)
