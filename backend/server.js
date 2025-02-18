f74fbb22ebd0556cd8c3cc0083e52737945c7e6c
Renée Gladys <renee16@live.se>
2025-02-18 16:50:33 +0100

    Mongodb atlas database connected and made folder models and file productModels.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const app = require ('./app')
+const connectDB = require ('./config/db.js')
+
+
+
+// setting up config file
 require('dotenv').config();
 
-
-
 const PORT = process.env.PORT || 5001
-
 console.log('PORT:', process.env.PORT);
 
+//connecting to database
+connectDB ()
 
 app.listen (PORT, () => {
     console.log (`server is running on PORT: ${PORT}`)
