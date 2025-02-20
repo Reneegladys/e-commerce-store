ad26f381d30ba14670367a154b30fd00a89e2f3f
Renée Gladys <renee16@live.se>
2025-02-20 14:07:36 +0100

    had to delete the package json and dependencies and do everything from beginning agangit status

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,19 @@
-
+const product = require ('../models/productModels')
 
 exports.getProducts = (req, res, next) => {
     res.status (200).json ({
         success: true,
         message: 'This route will show all products in database'
+    })
+}
+
+
+exports.newProduct = async (req, res, next) => {
+
+    const product = await product.create(req.body)
+    res.status (201).json({
+        success: true,
+        product
+
     })
 }
